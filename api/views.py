3bb351fa67c9600a6976cd4a74f64ea05d5a09f3
Mohamed207470 <mohamedboullah997@gmail.com>
2021-02-16 16:09:48 +0000

    3. Adding an API to an Existing Project -- videos 1 a 8

--- api/views.py
+++ api/views.py
@@ -1,7 +1,30 @@
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import generics, permissions
+from django.db import IntegrityError
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 
 from todo.models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodoCompleteSerializer
+
+
+@csrf_exempt
+def signupuser(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(data['username'], password=data['password'])
+            user.save()
+            login(request, user)
+            return JsonResponse({'token':'gjewijgipew'}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'That username has already been taken. Please choose a new username'},status=400)
+
 
 class TodoCompletedList(generics.ListAPIView):
     serializer_class = TodoSerializer
@@ -30,5 +53,17 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user)
+
+class TodoComplete(generics.UpdateAPIView):
+    serializer_class = TodoCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.datecompleted = timezone.now()
+        serializer.save()
 
 
